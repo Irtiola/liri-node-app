8a4927e399209b8dc1e08cc393f794d3a1b9f235
Irina K <irtiola@gmail.com>
2019-08-25 23:38:41 -0700

    works for concert-this +  installed moment

--- liri.js
+++ liri.js
@@ -4,48 +4,52 @@ var keys = require("./keys.js");//to read spotify id and secret from .env file
 var Spotify = require('node-spotify-api');//we need this to search for song 
 var fs = require('fs');//we need it to read from random.txt file
 var y = fs.readFileSync('./random.txt', 'utf8')//we use it with do-what-it-says command
-var spotify = new Spotify(keys.spotify);
-var queryAll = process.argv.slice(3).join(' ');
-var commands = process.argv[2];
-console.log(process.argv);
+var spotify = new Spotify(keys.spotify);//used get spotify key
+var moment = require('moment')
+var queryAll = process.argv.slice(3).join(' ');//query position in array
+var commands = process.argv[2];//command position in array
 
+console.log(process.argv);
+console.log(spotify);
 
 if (commands == 'concert-this') {
     apiCall = "https://rest.bandsintown.com/artists/" + queryAll + "/events?app_id=codingbootcamp";
     console.log(apiCall)
     allIn(apiCall, function (error, response, data) {
         console.log(data);
-        console.log('Venue: ' + JSON.parse(data).venue.name)
-        console.log('Location: ' + JSON.parse(data).venue.country)
-        console.log('Date of the event: ' + JSON.parse(data).datetime)
-
+        jp = JSON.parse(data);
+        console.log('Venue: ' + jp[0].venue.name)
+        console.log('Location: ' + jp[0].venue.country)
+        console.log('Date of the event: ' + jp[0].datetime)
+        jp[0].datetime = moment().format('MMMM Do YYYY')
     })
 } else if (commands == 'spotify-this-song') {
 
-    apiCall = "https://api.spotify.com/v1/search?query=" + queryAll + "&type=track&offset=20&limit=20";
+    // apiCall = "https://api.spotify.com/v1/search?query=" + queryAll + "&type=track&offset=20&limit=20";
     spotify.search({
-        type: 'track', query: queryAll, function(error, response, data) {
+        type: 'track', query: 'blink', function(error, response, data) {
             console.log('test')
-            console.log(error)
-            console.log(response)
+
         }
     })
 
 } else if (commands == 'movie-this') {
     apiCall = "https://www.omdbapi.com/?t=" + queryAll + "&y=&plot=short&apikey=trilogy";
     allIn(apiCall, function (error, response, data) {
-        console.log('Title: ' + JSON.parse(data).Title);
-        console.log('Year: ' + JSON.parse(data).Year);
-        console.log('imdbRating: ' + JSON.parse(data).imdbRating);
-        console.log('Rotten Tomatoes: ' + JSON.parse(data).RottenTomatoes);
-        console.log('Country: ' + JSON.parse(data).Country);
-        console.log('Language: ' + JSON.parse(data).Language);
-        console.log('Plot: ' + JSON.parse(data).Plot);
-        console.log('Actors: ' + JSON.parse(data).Actors);
+        jp = JSON.parse(data);
+        console.log('Title: ' + jp.Title);
+        console.log('Year: ' + jp.Year);
+        console.log('imdbRating: ' + jp.imdbRating);
+        console.log('Rotten Tomatoes: ' + jp.RottenTomatoes);
+        console.log('Country: ' + jp.Country);
+        console.log('Language: ' + jp.Language);
+        console.log('Plot: ' + jp.Plot);
+        console.log('Actors: ' + jp.Actors);
     })
 } else if (commands == "do-what-it-says") {
 
     console.log(y);
+
     apiCall = "https://api.spotify.com/v1/search?query=" + y + "&type=track&offset=20&limit=20"
     allIn(apiCall, function (error, response, data) {
         console.log(data)
